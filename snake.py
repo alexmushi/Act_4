49010f13205a2bb847119fb251ba73834e191b01
alexmushi <A01705550@tec.mx>
2024-05-08 17:24:15 -0600

    Se corrigen varios errores de formato de Flake8, se usó un formateador en línea

--- snake.py
+++ snake.py
@@ -1,33 +1,19 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
 from random import randrange
-from turtle import *
-
+from turtle import setup, hideturtle, tracer, update, clear, ontimer, listen, onkey, done
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
-
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
 
-
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
-
 
 def move():
     """Move snake forward one segment."""
@@ -56,7 +42,6 @@ def move():
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
-
 
 setup(420, 420, 370, 0)
 hideturtle()
